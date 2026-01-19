9f5cc2ead8665196f41073256d2a71153fa5fe6e
chandrathakur020 <chandrathakur3456@gmail.com>
2026-01-20 00:30:36 +0530

    Update top.java

--- top.java
+++ top.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+class top{
+  public static void main(string[]args){
+    int a,b,c;
+    Scanner sc = new Scanner (System.in);
+    System.out.println("Enter marks of chandra")
+      chandra=sc.nextInt();
+    Scanner scan= new Scannner (System.in);
+     System.out.println("Enter marks of Priya")
+      Priya=sc.nextInt();
+    Scanner scc= new Scannner (System.in);
+     System.out.println("Enter marks of cody")
+      cody=scc.nextInt();
+      double avg=(a+b+c)/3;
+    system.out.println("avg is"+avg);
 
+    if (avg>80)
+      System.out.println("Enter Grade A");
+    else if (avg >=60 && avg avg<=79)
+      System.out.println("Enter Grade B");
+    else 
+      System.out.println("Enter Faillll");
+    
+    
